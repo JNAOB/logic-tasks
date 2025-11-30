b57d9c0c013ba69b308dc8aeb885674087cb8b95
JNAOB <120253002+JNAOB@users.noreply.github.com>
2025-11-30 19:15:58 +0100

    only Syntrees with two Atoms

--- test/SynTreeSpec.hs
+++ test/SynTreeSpec.hs
@@ -55,7 +55,7 @@ validBoundsSynTreeConfig :: Gen SynTreeConfig
 validBoundsSynTreeConfig = do
   binOpFrequencies <- elements [opFrequencies, opFrequenciesNoArrows]
   maxConsecutiveNegations <- chooseInteger (0, 3)
-  let availableAtoms = "A" -- sublistOf ['A' .. 'Z'] `suchThat` (not . null)
+  let availableAtoms = "AB" -- sublistOf ['A' .. 'Z'] `suchThat` (not . null)
   minAmountOfUniqueAtoms <- chooseInteger (1, fromIntegral $ length availableAtoms)
   minNodes <- chooseInteger (max 3 (minAmountOfUniqueAtoms * 2), 60) `suchThat` \minNodes' -> maxConsecutiveNegations /= 0 || odd minNodes'
   let minDepth = 1 + floor (logBase (2 :: Double) $ fromIntegral minNodes)
