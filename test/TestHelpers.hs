b426fb0a74ea33368747ccf6103fe85527c66784
JNAOB <120253002+JNAOB@users.noreply.github.com>
2025-11-30 13:25:28 +0100

    genSublistOf uses suchThat

--- test/TestHelpers.hs
+++ test/TestHelpers.hs
@@ -12,7 +12,7 @@ import Control.OutputCapable.Blocks.Generic (evalLangM, RunnableOutputCapable (R
 import Control.Monad.Identity (Identity(runIdentity))
 import Data.Maybe (isJust)
 import Control.OutputCapable.Blocks (Language, LangM')
-import Test.QuickCheck (Gen, shuffle, chooseInt)
+import Test.QuickCheck (Gen, sublistOf, suchThat)
 
 deleteBrackets :: String  -> String
 deleteBrackets = filter (`notElem` "()")
@@ -33,5 +33,4 @@ doesNotRefuseIO thing = do
 
 genSublistOf :: (Int, Int) -> [a] -> Gen [a]
 genSublistOf (minLength, maxLength) xs = do
-  lengthAtoms <- chooseInt (minLength, min maxLength (length xs))
-  take lengthAtoms <$> shuffle xs
+  sublistOf xs `suchThat` \ys -> let len = length ys in len >= minLength && len <= maxLength
